8e636af7cb34bcd4f130104e316b92e3beb960f6
Hyrin-mansoor <hyrin@htsqatar.com>
2026-03-09 15:33:15 +0300

    added test button model config test

--- changai/changai/doctype/changai_settings/changai_settings.js
+++ changai/changai/doctype/changai_settings/changai_settings.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2026, ERpGulf and contributors
+// Copyright (c) 2026, ERPGulf and contributors
 // For license information, please see license.txt
 frappe.ui.form.on("ChangAI Settings", {
     refresh(frm) {
@@ -6,7 +6,7 @@ frappe.ui.form.on("ChangAI Settings", {
             frappe.call({
                 method: "changai.changai.api.v2.text2sql_pipeline_v2.download_model",
                 freeze: true,
-                freeze_message: "Downloading Model",
+                freeze_message: "Downloading Model...",
                 callback(r) {
                     if (!r.message) return;
                     frappe.show_alert({
@@ -23,39 +23,46 @@ frappe.ui.form.on("ChangAI Settings", {
                 }
             });
         });
-        frm.add_custom_button(__('test'), () => {
+
+        frm.add_custom_button(__('Test Model Config'), () => {
             frappe.call({
                 method: "changai.changai.api.v2.text2sql_pipeline_v2.debug_model_config",
-                callback: (r) => {
+                callback(r) {
+                    console.log(r.message);
+                    frappe.msgprint({
+                        title: __("Model Debug"),
+                        message: `<pre>${JSON.stringify(r.message, null, 2)}</pre>`,
+                        indicator: "blue"
+                    });
+                }
+            });
+        });
+
+        frm.add_custom_button(__('Update Master Data'), () => {
+            frappe.call({
+                method: "changai.changai.api.v2.auto_gen_api.sync_master_data_smart",
+                freeze: true,
+                freeze_message: "Updating Master Data...",
+                callback(r) {
+                    console.log(r.message);
+                }
+            });
+        });
+
+        frm.add_custom_button(__('Update Schema'), () => {
+            frappe.call({
+                method: "changai.changai.api.v2.auto_gen_api.sync_schema_and_enqueue_descriptions",
+                freeze: true,
+                freeze_message: "Syncing schema...",
+                callback(r) {
                     console.log(r.message);
                 }
             });
         });
     },
+
     create_train_data(frm) {
         create_data_from_selected_rows(frm);
-    },
-
-    update_masterdata_file(frm) {
-        frappe.call({
-            method: "changai.changai.api.v2.auto_gen_api.sync_master_data_smart",
-            freeze: true,
-            freeze_message: "Updating Master Data...",
-            callback(r) {
-                console.log(r.message);
-            }
-        });
-    },
-
-    update_schema_file(frm) {
-        frappe.call({
-            method: "changai.changai.api.v2.auto_gen_api.sync_schema_and_enqueue_descriptions",
-            freeze: true,
-            freeze_message: "Syncing schema...",
-            callback(r) {
-                console.log(r.message);
-            }
-        });
     }
 });
 
@@ -63,7 +70,6 @@ frappe.ui.form.on("ChangAI Settings", {
 function create_data_from_selected_rows(frm) {
     const table_field = "module_and_description";
     const grid = frm.fields_dict[table_field].grid;
-
     const selected_rows = grid.get_selected_children();
 
     if (!selected_rows.length) {
